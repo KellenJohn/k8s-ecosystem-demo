eed6eee4000131a049ac04a3847ec34e18491c99
Kellen <kelen175@hotmail.com>
2022-12-30 13:13:00 +0800

    Update k3s.sh
    Use K3d to install K3s

--- k3s.sh
+++ k3s.sh
@@ -1,8 +1,8 @@
-# k3s
+# k3s Deployment 
 # Curl
 curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 k3d cluster create demo --servers 1 --agents 3
 curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
-# kubectl
+# Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
