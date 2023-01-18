45b1dcb640a22577eecba89d19bb8c43f9b5bca9
Kellen <kelen175@hotmail.com>
2023-01-18 18:40:23 +0800

    Update dockerlab-online.sh

--- dockerlab-online.sh
+++ dockerlab-online.sh
@@ -9,9 +9,5 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 
 # install 
 k3d cluster create demo --servers 1 --agents 3
-
-# k3s 安裝
-curl -sfL https://get.k3s.io | sh - 
-# Check for Ready node, takes ~30 seconds 
-k3s kubectl get node 
+kubectl get node 
 
