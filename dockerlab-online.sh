ca36e136e56e62c0025f9788393f87680830e9b1
Kellen <kelen175@hotmail.com>
2023-01-19 13:27:48 +0800

    Update dockerlab-online.sh

--- dockerlab-online.sh
+++ dockerlab-online.sh
@@ -1,13 +1,13 @@
 # prepare online dockerlab package
 apk update
 apk add curl
+# kubectl install
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # download k3d/k3s
-# curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
+curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 
 # install 
 k3d cluster create demo --servers 1 --agents 3
 kubectl get node 
-
