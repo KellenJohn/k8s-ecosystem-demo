3fd2e865faaaa1a033b41ac689f1ebfbfe5f9db3
Kellen <kelen175@hotmail.com>
2023-02-18 15:39:09 +0800

    Update dockerlab-online.sh

--- dockerlab-online.sh
+++ dockerlab-online.sh
@@ -4,11 +4,15 @@ apk add curl
 # download k3d/k3s
 curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 
-# install 
-k3d cluster create demo --servers 1 --agents 3 \
-kubectl get node 
-
 # kubectl install
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"  \
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# install 
+k3d cluster create demo --servers 1 --agents 3 \
+kubectl get node 
+
+# ArgoCD
+kubectl create namespace argocd \ 
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml \
+kubectl get pods -n argocd
