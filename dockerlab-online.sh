e0ac1c930efb4f2649073a565c1268cb8e5891d9
Kellen <kelen175@hotmail.com>
2023-02-18 17:58:17 +0800

    Update dockerlab-online.sh

--- dockerlab-online.sh
+++ dockerlab-online.sh
@@ -22,3 +22,33 @@ kubectl get pods -n argocd
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
+
+##############################################################################################
+# Good
+https://icloudnative.io/posts/getting-started-with-argocd/
+https://www.readfog.com/a/1676342497667813376
+https://rammusxu.github.io/2019/07/16/GitOps-Argo-CD-Note/
+https://blog.51cto.com/u_15315026/5078430
+
+
+https://stackoverflow.com/questions/67262769/access-argocd-server
+https://stackoverflow.com/questions/71030111/how-to-login-to-argocd-cli-non-interactive-in-ci-like-github-actions
+
+argocd login 10.43.62.196:31214 --username admin --password LA-a5ik06Bip74qt --insecure
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+argoPass=$(kubectl -n argocd get secret argocd-initial-admin-secret \
+    -o jsonpath="{.data.password}" | base64 -d)
+
+argocd login --insecure --grpc-web k3s_master:32761 --username admin \
+    --password $argoPass
+    
+#     
+argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+# Obtaining the ArgoCD server's hostname is also no big deal using:
+
+kubectl get service argocd-server -n argocd --output=jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+argocd login $(kubectl get service argocd-server -n argocd --output=jsonpath='{.status.loadBalancer.ingress[0].hostname}') \
+       --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --insecure
+    
